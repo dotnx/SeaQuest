c01f6db1761939ccfe009dfb277266960533687d
Michael Fettin <38879134+dotnx@users.noreply.github.com>
2025-05-10 16:09:58 +0200

    chore(course): complete lesson 20 - Finishing Enemy Spawning System
    Lesson 19 was a overview of array datatypes in Godot, so no code changes were made.

--- seaquest-udemy-tutorial/enemies/enemy_spawner/enemy_spawner.gd
+++ seaquest-udemy-tutorial/enemies/enemy_spawner/enemy_spawner.gd
@@ -2,15 +2,29 @@ extends Node2D
 
 const Shark = preload("res://enemies/shark/shark.tscn")
 
+var used_spawn_points = []
+
 @onready var left = $Left
 @onready var right = $Right
 
+# new wave spawned
 func _on_spawn_enemy_timer_timeout():
 	for i in range(4):
 		spawn_enemy()
 	
+	used_spawn_points.clear()
+
+# just a one enemy spawn
 func spawn_enemy():
-	var rand_spawn_point_number = randi_range(1, 4)
+	var available_spawn_points: Array = []
+	
+	for i in range(1, 5):
+		if !used_spawn_points.has(i):
+			available_spawn_points.append(i)
+	
+	var rand_spawn_point_index = randi_range(0, available_spawn_points.size() - 1)
+	var selected_spawn_point_number = available_spawn_points[rand_spawn_point_index]
+	used_spawn_points.append(selected_spawn_point_number)
 	
 	var selected_side_node = left
 	var spawn_right = bool(randi_range(0, 1))
@@ -18,7 +32,7 @@ func spawn_enemy():
 	if spawn_right == true:
 		selected_side_node = right
 	
-	var selected_spawn_point = selected_side_node.get_node(str(rand_spawn_point_number))
+	var selected_spawn_point = selected_side_node.get_node(str(selected_spawn_point_number))
 	var spawn_position = selected_spawn_point.global_position
 	
 	var shark_instance = Shark.instantiate()

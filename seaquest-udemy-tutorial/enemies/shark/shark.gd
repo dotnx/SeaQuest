32ee267fc62c768fd761de18c03dc1718348f9b5
Michael Fettin <38879134+dotnx@users.noreply.github.com>
2025-05-10 13:59:50 +0200

    chore(course): complete lesson 16 - Enemy Spawning Direction Fix & Randomness

--- seaquest-udemy-tutorial/enemies/shark/shark.gd
+++ seaquest-udemy-tutorial/enemies/shark/shark.gd
@@ -6,9 +6,15 @@ const MOVEMENT_AMPLITUDE = 0.5
 
 var velocity = Vector2(1, 0)
 
+@onready var sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
 	velocity.y = sin(global_position.x * MOVEMENT_FREQUENCY) * MOVEMENT_AMPLITUDE
 	global_position += velocity * SPEED * delta
+
+func flip_direction():
+	velocity = -velocity
+	sprite.flip_h = !sprite.flip_h
 
 func _on_area_entered(area):
 	if area.is_in_group("PlayerBullet"):

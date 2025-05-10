ba348822eff2560205d599486fc3dd8f5102f8a1
Michael Fettin <38879134+dotnx@users.noreply.github.com>
2025-05-10 12:22:52 +0200

    chore(course): complete lesson 8 - Player Shooting

--- seaquest-udemy-tutorial/player/player.gd
+++ seaquest-udemy-tutorial/player/player.gd
@@ -1,7 +1,9 @@
 extends AnimatedSprite2D
 
 var velocity = Vector2(0, 0)
+
 const SPEED = Vector2(125, 90)
+const Bullet = preload("res://player/player_bullet/player_bullet.tscn")
 
 func _process(delta):
 	velocity.x = Input.get_axis("move_left", "move_right")
@@ -13,6 +15,11 @@ func _process(delta):
 		flip_h = false
 	elif velocity.x < 0:
 		flip_h = true
+	
+	if Input.is_action_just_pressed("shoot"):
+		var bullet_instance = Bullet.instantiate()
+		bullet_instance.global_position = global_position
+		get_tree().current_scene.add_child(bullet_instance)
 
 func _physics_process(delta):
 	global_position += velocity * SPEED * delta

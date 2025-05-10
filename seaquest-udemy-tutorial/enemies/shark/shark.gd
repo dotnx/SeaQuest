a705acd35109c27ee306281e8ee70bcbf64bbf93
Michael Fettin <38879134+dotnx@users.noreply.github.com>
2025-05-10 13:39:27 +0200

    chore(course): complete lesson 14 - Scene Organization and Area Fixes

--- seaquest-udemy-tutorial/enemies/shark/shark.gd
+++ seaquest-udemy-tutorial/enemies/shark/shark.gd
@@ -1,4 +1,4 @@
-extends AnimatedSprite2D
+extends Area2D
 
 const SPEED = 50
 const MOVEMENT_FREQUENCY = 0.15
@@ -10,6 +10,7 @@ func _physics_process(delta):
 	velocity.y = sin(global_position.x * MOVEMENT_FREQUENCY) * MOVEMENT_AMPLITUDE
 	global_position += velocity * SPEED * delta
 
-func _on_hitbox_area_entered(area):
-	area.get_parent().queue_free()
-	queue_free()
+func _on_area_entered(area):
+	if area.is_in_group("PlayerBullet"):
+		area.queue_free()
+		queue_free()

c2df572582b6c8f0a240e4c4f3e831bd6c0bf703
Michael Fettin <38879134+dotnx@users.noreply.github.com>
2025-05-11 18:33:56 +0200

    chore(course): complete lesson 43 - Restarting the Game

--- seaquest-udemy-tutorial/user_interface/game_over/game_over.gd
+++ seaquest-udemy-tutorial/user_interface/game_over/game_over.gd
@@ -1,13 +1,20 @@
 extends Control
 
 @onready var current_score_label = $CurrentScoreLabel
+@onready var game_over_delay_timer = $GameOverDelay
 
 func _ready():
 	GameEvent.connect("game_over", Callable(self, "_activate_game_over"))
 	
 	visible = false
 
+func _process(delta):
+	if Input.is_action_just_pressed("shoot") and visible == true:
+		get_tree().reload_current_scene()
+
 func _activate_game_over():
 	current_score_label.text = "Score " + str(Global.current_points)
-	
+	game_over_delay_timer.start()
+
+func _on_game_over_delay_timeout():
 	visible = true

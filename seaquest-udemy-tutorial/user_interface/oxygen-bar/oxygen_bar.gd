9f9212fdf97e1c9cc96e7389addb110c409ad21a
Michael Fettin <38879134+dotnx@users.noreply.github.com>
2025-05-11 21:05:53 +0200

    chore(course): complete lesson 50 - Shaking the Oxygen Bar

--- seaquest-udemy-tutorial/user_interface/oxygen-bar/oxygen_bar.gd
+++ seaquest-udemy-tutorial/user_interface/oxygen-bar/oxygen_bar.gd
@@ -1,4 +1,38 @@
-extends TextureProgressBar
+extends Node2D
+
+var previous_amount = 0
+
+@onready var progress_bar = $TextureProgress
 
 func _process(delta):
-	value = Global.oxygen_level
+	progress_bar.value = Global.oxygen_level
+	
+	var amount_rounded = round(Global.oxygen_level)
+	
+	if amount_rounded == previous_amount:
+		return
+		
+	if amount_rounded == 25:
+		alert(1.25, 5)
+	elif amount_rounded == 15:
+		alert(1.3, 7)
+	elif amount_rounded == 10:
+		alert(1.35, 10)
+	elif amount_rounded == 7:
+		alert(1.4, 15)
+	elif amount_rounded == 5:
+		alert(1.5, 20)
+	elif amount_rounded == 2:
+		alert(1.6, 25)
+	elif amount_rounded == 1:
+		alert(1.8, 35)
+	
+	previous_amount = amount_rounded
+
+func _physics_process(delta):
+	scale = lerp(scale, Vector2(1, 1), 6 * delta)
+	rotation_degrees = lerp(rotation_degrees, 0.0, 6 * delta)
+
+func alert(scale_value, rotation_value):
+	scale = Vector2(scale_value, scale_value)
+	rotation_degrees = randf_range(-rotation_value, rotation_value)
